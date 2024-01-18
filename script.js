0f581158de221d293c57cf26ec777d684be4f9b0
codedstrings <mridhulml@gmail.com>
2024-01-18 17:26:35 +0530

    add grid-size-btn

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 const mainContainer = document.querySelector(".main-container");
 const drawingBoard = document.querySelector(".drawing-board");
-let gridSize = 100;
+const gridSizeBtn = document.querySelector(".grid-size-btn");
+let gridSize = 16;
+let pageLoaded = false;
 
 function drawBoard() {
   console.log("drawing board");
@@ -21,15 +23,27 @@ function drawBoard() {
     drawingBoard.appendChild(gridRow);
   }
   drawingBoard.removeEventListener("click", drawBoard);
+  pageLoaded = true;
 }
-
-drawingBoard.addEventListener("click", drawBoard);
+//entry point
+drawingBoard.addEventListener("click", pageActive);
+//pageActive draws the first grid and activates control board buttons
+function pageActive() {
+  //draw default 16x16 drawingboard
+  drawBoard();
+  //activate grid size change button
+  gridSizeBtn.addEventListener("click", () => {
+    gridSize = prompt("What's the grid size you want?");
+    if (gridSize >= 16 && gridSize <= 100) {
+      drawBoard();
+    } else alert("Enter a grid size between 16-100");
+  });
+}
 
 function startDrawing(event) {
   // Access the gridBox that the event has fired on
   const gridBox = event.target;
   gridBox.classList.add("colored-grid");
-  //remove event listener after coloring. 
-  gridBox.removeEventListener("mouseover",startDrawing);
+  //remove event listener after coloring.
+  gridBox.removeEventListener("mouseover", startDrawing);
 }
-
