991edf7d4e2afd2fa4edd4475151b503280ed200
codedstrings <mridhulml@gmail.com>
2024-01-18 15:22:40 +0530

    remove explicit declaration of grid-box height and width; use flex: 1 instead

--- script.js
+++ script.js
@@ -1,22 +1,26 @@
-const mainContainer = document.querySelector('.main-container');
-const drawingBoard = document.querySelector('.drawing-board');
-let gridSize = 50;
+const mainContainer = document.querySelector(".main-container");
+const drawingBoard = document.querySelector(".drawing-board");
+let gridSize = 16;
 
 function drawBoard() {
   console.log("drawing board");
-  drawingBoard.classList.remove('inactive');
-  drawingBoard.classList.add('active');
-  drawingBoard.style.setProperty('--gridSize', gridSize);
-  drawingBoard.innerHTML = '';
+  drawingBoard.classList.remove("inactive");
+  drawingBoard.classList.add("active");
+  drawingBoard.style.setProperty("--gridSize", gridSize);
+  drawingBoard.innerHTML = "";
 
-  for (var i = 0; i < gridSize * gridSize; i++) {
-    var gridBox = document.createElement('div');
-    gridBox.className = 'grid-box';
-    drawingBoard.appendChild(gridBox);
+  for (let i = 0; i < gridSize; i++) {
+    let gridRow = document.createElement("div");
+    gridRow.className = "grid-row";
+    for (let j = 0; j < gridSize; j++) {
+      let gridBox = document.createElement("div");
+      gridBox.className = "grid-box";
+      gridRow.appendChild(gridBox);
+    }
+    drawingBoard.appendChild(gridRow);
   }
 
-  drawingBoard.removeEventListener('click', drawBoard);
+  drawingBoard.removeEventListener("click", drawBoard);
 }
 
-drawingBoard.addEventListener('click', drawBoard);
-
+drawingBoard.addEventListener("click", drawBoard);
